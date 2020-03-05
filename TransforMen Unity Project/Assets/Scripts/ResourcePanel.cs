97b983544e0a5a97c3ae7e18ee53709d00384960
Nathan Marshall <NathanMarshall@cmail.carleton.ca>
2020-03-05 14:51:28 -0500

    Resources are now displayed in the UI

---
+++ TransforMen Unity Project/Assets/Scripts/ResourcePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePanel : MonoBehaviour
+{
+    public PlayerResources resources;
+    public TMPro.TextMeshProUGUI populationDisplay;
+    public TMPro.TextMeshProUGUI scrapDisplay;
+    public TMPro.TextMeshProUGUI spikesDisplay;
+    public TMPro.TextMeshProUGUI crawlbitsDisplay;
+
+    void OnGUI() {
+        populationDisplay.SetText(resources.GetPopulationResource().ToString());
+        scrapDisplay.SetText(resources.GetScrapResource().ToString());
+        spikesDisplay.SetText(resources.GetSpikeResource().ToString());
+        crawlbitsDisplay.SetText(resources.GetCrawlbitResource().ToString());
+    }
+}
