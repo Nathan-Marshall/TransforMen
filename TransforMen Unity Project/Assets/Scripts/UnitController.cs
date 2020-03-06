07ecef57d3f8435ffe9cb9247f96bf261dd0e080
Megan Perera <megan.perera1406@gmail.com>
2020-03-06 15:26:20 -0500

    Updated models and added textures. Updated prefabs

--- TransforMen Unity Project/Assets/Scripts/UnitController.cs
+++ TransforMen Unity Project/Assets/Scripts/UnitController.cs
@@ -8,7 +8,8 @@ public class UnitController : MonoBehaviour
     {
         Ruin,
         Ally,
-        TransformStation
+        TransformStation, 
+        Enemy
     }
 
     // Start is called before the first frame update
