d15712650e6c21c728389e7d102ee7397af9e57d
Mitchell Blanchard <mitchellblanchard3@gmail.com>
2020-03-15 19:31:55 -0400

    Fixed the weird lean-in thing. You can also now change directions on the fly. There is a weird bug where the unit jumps slightly that I will have to fix.

--- TransforMen Unity Project/Assets/Scripts/IndividualMovement.cs
+++ TransforMen Unity Project/Assets/Scripts/IndividualMovement.cs
@@ -8,6 +8,8 @@ public class IndividualMovement : MonoBehaviour
     public Transform initialTransform;
     public Vector3 initialPosition;
 
+    IEnumerator moveRoutine = null;
+
     public bool toMove;
     public System.Action actionOnArrival;
     private float speed = 40.0f;
@@ -34,14 +36,24 @@ public class IndividualMovement : MonoBehaviour
             initialTransform = transform;
             initialPosition = transform.position;
 
-            StartCoroutine("Move");
+            if (moveRoutine == null)
+            {
+                moveRoutine = Move();
+                StartCoroutine(moveRoutine);
+            }
+            else
+            {
+                StopCoroutine(moveRoutine);
+                moveRoutine = Move();
+                StartCoroutine(moveRoutine);
+            }
+
             toMove = false;
         }
     }
 
     Vector3 InterpPosition(float t)
     {
-        // Clamp parameter to the valid range
         if (t < 0.0f) { t = 0.0f; }
         if (t > 1.0f) { t = 1.0f; }
 
@@ -51,7 +63,6 @@ public class IndividualMovement : MonoBehaviour
             return destination;
         }
 
-        // Compute Catmull-Rom spline
         float s2 = Mathf.Pow(t, 2);
         float s3 = Mathf.Pow(t, 3);
         Vector3 pos = (2 * s3 - 3 * s2 + 1) * initialPosition +
@@ -63,35 +74,31 @@ public class IndividualMovement : MonoBehaviour
 
     IEnumerator Move()
     {
-        // Move ship along the path by interpolating spline from 0 to 1
         for (float s = 0.0f; s < 1.0f; s += 0.01f)
         {
-            // Evaluate spline to get the position
             Vector3 newPos = InterpPosition(s);
             this.transform.position = new Vector3(newPos.x, this.transform.position.y, newPos.z);
 
-            // Get orientation from tangent along the curve
             Vector3 curve_tan = InterpPosition(s + 0.01f) - InterpPosition(s);
             curve_tan.Normalize();
-            // Check if we are close to the last point along the path
+
             if (s >= 0.99f)
             {
-                // The last point does not have a well-defined tangent, so use the one of the curve
                 curve_tan = new Vector3(0, 0, -1); ;
             }
-            // Create orientation from the tangent
+
+
             Quaternion orient = new Quaternion();
-            orient.SetLookRotation(curve_tan, Vector3.up);
-            // The following rotations are needed to set the proper initial orientation of the ship
+            orient.SetLookRotation(new Vector3(curve_tan.x, 0, curve_tan.z), Vector3.up);
             orient *= Quaternion.AngleAxis(180, Vector3.forward);
             orient *= Quaternion.AngleAxis(180, Vector3.right);
             orient *= Quaternion.AngleAxis(270, Vector3.up);
 
-            // Set ship's orientation
             this.transform.rotation = orient;
             yield return new WaitForSeconds(0.05f);
         }
         this.transform.position = destination;
+        this.transform.position = new Vector3(destination.x, this.transform.position.y, destination.z);
 
         if (actionOnArrival != null)
         {
