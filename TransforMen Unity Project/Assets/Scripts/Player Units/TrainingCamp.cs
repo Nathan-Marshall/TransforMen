375522b0bcd25bc4d7155af0bd03524ef1530732
Nathan-Marshall <nathanmarshall@cmail.carleton.ca>
2020-03-30 20:43:26 -0400

    Revert "Training camp can be placed and can spawn dudes (not neatly tho)"

--- TransforMen Unity Project/Assets/Scripts/Player Units/TrainingCamp.cs
+++ TransforMen Unity Project/Assets/Scripts/Player Units/TrainingCamp.cs
@@ -15,21 +15,7 @@ public class TrainingCamp : StaticUnit
     //COST: 5 scrap
 
     //Can train infantry 
-    public GameObject infantryPrefab; 
     //Consumes resources 
-    public int populationCost;
-    public int scrapCost;
-    private PlayerResources resources;
-    private Collider boxCollider; 
-
-    public bool Affordable
-    {
-        get
-        {
-            return resources.GetPopulationResource() >= populationCost
-                    && resources.GetScrapResource() >= scrapCost;
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,34 +23,11 @@ public class TrainingCamp : StaticUnit
         SetToPlayerTeam();
         SetSelectable(true);
         SetDescription(desc);
-
-        resources = GameObject.Find("Game Control").GetComponent<PlayerResources>();
-        boxCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-
-        if (boxCollider.Raycast(ray, out RaycastHit hit, 10000.0f))
-        { 
-            //If the building is clicked and can afford, make a new unit 
-            if (Input.GetMouseButtonDown(0) && Affordable)
-            {
-                //Spawn a guy
-                MakeInfantry(); 
-            }
-        }
-    }
-
-    void MakeInfantry()
-    {
-        resources.SpendPopulation(populationCost);
-        resources.SpendScrap(scrapCost);
-
-        Transform spawnLocation = transform.Find("SpernPernt");
-        Vector3 spawnVec = spawnLocation.position; 
-        GameObject infantry = Instantiate(infantryPrefab, spawnVec, Quaternion.identity);
+        
     }
 }
